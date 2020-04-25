679baa2e1f25e9571a155815247fe29c361af6cd
Alberto Costa <alberto.costa05@gmail.com>
2020-04-25 17:36:48 +0200

    Add test_output_update

--- tests/test_outputs.py
+++ tests/test_outputs.py
@@ -12,7 +12,7 @@ def outputs():
     return Outputs([NoteOutput(0), GateOutput(1, 72), CvOutput(0, 2)])
 
 
-def make_note_on_messages(channel, note):
+def make_note_on_message(channel, note):
     return Message.from_dict({'type': 'note_on',
                               'time': 0,
                               'note': note,
@@ -20,34 +20,57 @@ def make_note_on_messages(channel, note):
                               'channel': channel})
 
 
-def make_cc_messages(channel, control):
+def make_cc_message(channel, control):
     return Message.from_dict({'type': 'control_change',
                               'time': 0,
                               'control': control,
-                              'value': 60,
+                              'value': 64,
                               'channel': channel})
 
 
 def test_relevant_msg(outputs):
-    note_on_messages = [make_note_on_messages(0, n) for n in [0, 64, 127]]
-    note_on_messages.append(make_note_on_messages(1, 72))
+    note_on_messages = [make_note_on_message(0, n) for n in [0, 64, 127]]
+    note_on_messages.append(make_note_on_message(1, 72))
 
     for m in note_on_messages:
         assert outputs.filter(m)
 
-    cc_messages = [make_cc_messages(0, 2)]
+    cc_messages = [make_cc_message(0, 2)]
 
     for m in cc_messages:
         assert outputs.filter(m)
 
 
 def test_non_relevant_msg(outputs):
-    note_on_messages = [make_note_on_messages(1, n) for n in [0, 64, 127]]
+    note_on_messages = [make_note_on_message(1, n) for n in [0, 64, 127]]
 
     for m in note_on_messages:
         assert not outputs.filter(m)
 
-    cc_messages = [make_cc_messages(0, n) for n in [0, 64, 127]]
+    cc_messages = [make_cc_message(0, n) for n in [0, 64, 127]]
 
     for m in cc_messages:
         assert not outputs.filter(m)
+
+
+def test_output_update(outputs):
+    note_message = make_note_on_message(0, 64)
+    outputs.update(note_message)
+
+    assert outputs.outputs[0].output_status[0] == 0.5
+    assert outputs.outputs[0].output_status[1] == 0.8
+
+    assert outputs.outputs[1].output_status[0] == 0
+
+    gate_message = make_note_on_message(1, 72)
+    outputs.update(gate_message)
+
+    assert outputs.outputs[0].output_status[0] == 0.5
+    assert outputs.outputs[0].output_status[1] == 0.8
+
+    assert outputs.outputs[1].output_status[0] == 1
+
+    cc_message = make_cc_message(0, 2)
+    outputs.update(cc_message)
+
+    assert outputs.outputs[2].output_status[0] == 0.5
