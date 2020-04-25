cd8757fb59d73402a446bc4d96e9f45aebc62825
Alberto Costa <alberto.costa05@gmail.com>
2020-04-25 17:15:18 +0200

    Add test_outputs

---
+++ tests/test_outputs.py
@@ -0,0 +1,35 @@
+import mido
+import pytest
+
+from cvpy.outputs import Outputs
+from cvpy.outputs.types import NoteOutput, GateOutput, CvOutput
+
+from mido.messages.messages import Message
+
+
+@pytest.fixture
+def outputs():
+    return Outputs([NoteOutput(0), GateOutput(1, 72), CvOutput(0, 2)])
+
+
+def make_note_on_messages(channel, note):
+    return Message.from_dict({'type': 'note_on',
+                              'time': 0,
+                              'note': note,
+                              'velocity': 60,
+                              'channel': channel})
+
+
+def test_relevant_msg(outputs):
+    note_on_messages = [make_note_on_messages(0, n) for n in [0, 64, 127]]
+    note_on_messages.append(make_note_on_messages(1, 72))
+
+    for m in note_on_messages:
+        assert outputs.filter(m)
+
+
+def test_non_relevant_msg(outputs):
+    note_on_messages = [make_note_on_messages(1, n) for n in [0, 64, 127]]
+
+    for m in note_on_messages:
+        assert not outputs.filter(m)
