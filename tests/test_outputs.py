c38a1b9d90aaf2a0095b19aa9f35d86a3491de51
Alberto Costa <alberto.costa05@gmail.com>
2020-04-25 17:27:47 +0200

    Add cc to tests

--- tests/test_outputs.py
+++ tests/test_outputs.py
@@ -20,6 +20,14 @@ def make_note_on_messages(channel, note):
                               'channel': channel})
 
 
+def make_cc_messages(channel, control):
+    return Message.from_dict({'type': 'control_change',
+                              'time': 0,
+                              'control': control,
+                              'value': 60,
+                              'channel': channel})
+
+
 def test_relevant_msg(outputs):
     note_on_messages = [make_note_on_messages(0, n) for n in [0, 64, 127]]
     note_on_messages.append(make_note_on_messages(1, 72))
@@ -27,9 +35,19 @@ def test_relevant_msg(outputs):
     for m in note_on_messages:
         assert outputs.filter(m)
 
+    cc_messages = [make_cc_messages(0, 2)]
+
+    for m in cc_messages:
+        assert outputs.filter(m)
+
 
 def test_non_relevant_msg(outputs):
     note_on_messages = [make_note_on_messages(1, n) for n in [0, 64, 127]]
 
     for m in note_on_messages:
+        assert not outputs.filter(m)
+
+    cc_messages = [make_cc_messages(0, n) for n in [0, 64, 127]]
+
+    for m in cc_messages:
         assert not outputs.filter(m)
